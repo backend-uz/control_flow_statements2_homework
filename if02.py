10f0c8491aa6fe0917e2020d99db2e0d4863b247
user1 <backenduz@gmail.com>
2022-11-22 23:07:23 +0500

    done

--- if02.py
+++ if02.py
@@ -8,4 +8,15 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+
+    if a < b:
+        if a < c:
+            return a
+        else:
+            return c
+    else:
+        if b < c:
+            return b
+        else:
+            return c
+print(main(2,4,6))
